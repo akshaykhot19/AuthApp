7d5adb6b1c7b0d2daff7aef258d98969f340e896
AkshayKhot1998 <akshay.khot.cdc@nmims.edu>
2025-05-09 17:03:30 +0530

    Initial commit

--- controller/Auth.js
+++ controller/Auth.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcrypt');
 const user = require('../model/user');
+const jwt = require('jsonwebtoken');
 
 //Signup route Handler
 exports.signup = async (req, res) => {
@@ -49,3 +50,66 @@ exports.signup = async (req, res) => {
         success: false,
         message: 'User cannot be registered , Please try again later' });
     }}
+
+
+//Login route Handler
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({ 
+        success: false,
+        message: 'Email and password are required' });
+    }
+    // Check if user exists
+    const existingUser = await user.find({ email });
+    if (existingUser.length === 0) {
+      return res.status(400).json({ 
+        success: false,
+        message: 'Invalid email or password' });
+    }
+    // Compare the password with the hashed password
+    const isPasswordValid = await bcrypt.compare(password, existingUser[0].password); 
+    if (!isPasswordValid) {
+      return res.status(400).json({ 
+        success: false,
+        message: 'Invalid email or password' });
+    }
+
+     // Generate a JWT token
+     const token = jwt.sign(
+      {email: existingUser[0].email ,id: existingUser[0]._id, role: existingUser[0].role }, // Payload
+      process.env.JWT_SECRET, // Secret key (store this in your .env file)
+      { expiresIn: '1h' } // Token expiration time
+    );
+
+    // Setting the token in the user object
+    // This is just for demonstration purposes. In a real application, you would send the token to the client.
+    //user.token = token;
+    //user.password = undefined; // Remove the password from the user object before sending it to the client
+
+    res.cookie('token', token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production', // Set to true if using HTTPS
+      expires : new Date (Date.now() + 3 *24 *60 *60 *1000) // 3 days expiration
+    }).status(200).json({ 
+      success: true,
+      message: 'Login successful',
+      token, // Send the token to the client
+      user: {
+        id: existingUser[0]._id,
+        name: existingUser[0].name,
+        email: existingUser[0].email,
+        role: existingUser[0].role
+      }
+    }); 
+
+  }
+  catch (error) {
+    console.error('Error during login:', error);
+    return res.status(500).json({ 
+      success: false,
+      message: 'Internal server error' });
+  } 
+}
--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const {login,signup} = require('../controller/Auth');
 
-//router.post('/login', login);
+router.post('/login', login);
 
 router.post('/signup', signup);
 
